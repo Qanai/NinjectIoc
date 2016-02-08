a7a878ac6f680e8e920dc8dc6ef2ecd53a6d27bf
Eliyahu Shlieserman <eliyahushli@gmail.com>
2016-02-08 22:09:04 +0200

    Registering

--- Ninject.ConsoleApp/Program.cs
+++ Ninject.ConsoleApp/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Credit;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +11,13 @@ namespace Ninject.ConsoleApp
     {
         static void Main(string[] args)
         {
+            var kerner = new StandardKernel();
+            kerner.Bind<ICreditCard>().To<MasterCard>();
+
+            var shopper = kerner.Get<Shopper>();
+
+            Console.WriteLine(shopper.Charge());
+            Console.Read();
         }
     }
 }
